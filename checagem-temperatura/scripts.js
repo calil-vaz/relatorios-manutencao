af130635e024100e12a86101a3d5a2de8f5fa6c7
calil-vaz <calilvazdev@gmail.com>
2025-01-27 14:18:32 -0300

    Update scripts.js

--- checagem-temperatura/scripts.js
+++ checagem-temperatura/scripts.js
@@ -208,7 +208,7 @@ function generatePDF() {
 
   html2pdf()
     .set({
-      margin: [25, 0, 15, 0], 
+      margin: [28, 0, 15, 0], 
       html2canvas: { scale: 2 },
       jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
       pagebreak: { mode: ["css", "legacy"] },
