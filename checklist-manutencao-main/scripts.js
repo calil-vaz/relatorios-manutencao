46685590c5f9fe30a5efe266f6d0782973ffcac5
calil-vaz <calilvazdev@gmail.com>
2025-02-16 17:05:31 -0300

    Update scripts.js

--- checklist-manutencao-main/scripts.js
+++ checklist-manutencao-main/scripts.js
@@ -275,7 +275,7 @@ function App() {
 
   html2pdf()
     .set({
-      margin: [25, 0, 20, 0],
+      margin: [30, 0, 20, 0],
       html2canvas: { scale: window.devicePixelRatio > 1 ? 3 : 2 },
       jsPDF: { format: "a4", orientation: "portrait" },
       pagebreak: { mode: ["css", "legacy"] },
