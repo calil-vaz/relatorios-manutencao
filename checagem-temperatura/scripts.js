59b7da7b42c539c3a1bcd621449892bb96658233
calil-vaz <calilvazdev@gmail.com>
2025-02-16 16:29:48 -0300

    Update scripts.js

--- checagem-temperatura/scripts.js
+++ checagem-temperatura/scripts.js
@@ -7,7 +7,7 @@ var content = document.getElementById("content");
 const savedProfile = localStorage.getItem("savedProfile");
 const valores = JSON.parse(savedProfile);
 
-content.style.height = "963px";
+content.style.height = "950px";
 
 function goBack() {
   window.history.back();
